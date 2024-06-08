307409a9f96d55b68f8d764261eb123b7d6f562f
danhazeltine <169066079+danhazeltine@users.noreply.github.com>
2024-06-08 17:48:18 +0100

    Update ClientOnly.tsx

--- components/ClientOnly.tsx
+++ components/ClientOnly.tsx
@@ -43,7 +43,19 @@ const ClientOnly: React.FC<{
     <>
       {children}
 
-      <script src="//code.jivosite.com/widget/BjSM8hU42b" async></script>
+      <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66648adb9a809f19fb3b792a/1hvsbsre6';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
       
     </>
   );
