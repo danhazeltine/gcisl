66db1a6da6cdeb5f371534ca2c79d616b1a4bf52
danhazeltine <169066079+danhazeltine@users.noreply.github.com>
2025-05-12 22:51:34 +0100

    Update TransactionEmail.tsx

--- email-templates/TransactionEmail.tsx
+++ email-templates/TransactionEmail.tsx
@@ -241,7 +241,7 @@ export const TransactionEmail = (props: TransactionInfoProp) => {
           <Text style={{ textAlign: "center", color: "#706a7b" }}>
             © {transaction?.createdAt?.getFullYear()} {company.name}, All Rights
             Reserved <br />
-            85 Great Portland Street, London, W1W 7LT, England, United Kingdom
+            Rue de Hesse 12, 1204 Genève, Switzerland
           </Text>
         </Section>
       </Body>
