b6c85b0262ad493000062da3058601ee557ea043
danhazeltine <169066079+danhazeltine@users.noreply.github.com>
2025-05-13 16:13:11 +0100

    Update ClientOnly.tsx

--- components/ClientOnly.tsx
+++ components/ClientOnly.tsx
@@ -3,7 +3,6 @@ import React, { useCallback, useEffect, useState } from "react";
 import useTheme from "./hooks/useTheme";
 import { Loader } from "@mantine/core";
 import useCompany from "./hooks/useCompany";
-import Script from "next/script";
 
 const ClientOnly: React.FC<{
   children: React.ReactNode;
@@ -31,7 +30,10 @@ const ClientOnly: React.FC<{
 
   if (!company || !isReady) {
     return (
-      <div className="h-[100vh] w-full flex items-center justify-center bg-black">
+      <div
+        className="h-[100vh] w-full flex items-center 
+        justify-center bg-black"
+      >
         <Loader color="#ffffff" />
       </div>
     );
@@ -40,43 +42,19 @@ const ClientOnly: React.FC<{
   return (
     <>
       {children}
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/682356c9470adc190e4b8f75/1ir50m1at';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
       
-      {/* JivoChat */}
-      <Script 
-        src="//code.jivochat.com/widget/BjSM8hU42b"
-        strategy="afterInteractive"
-      />
-      
-      {/* Tawk.to Direct Embed */}
-      <Script id="tawk-to" strategy="afterInteractive">
-        {`
-          var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-          Tawk_API.visitor = {
-            name: '${companyData.name?.replace(/'/g, "\\'") || 'Visitor'}',
-            email: '${companyData.email?.replace(/'/g, "\\'") || ''}',
-            hash: '${companyData.id?.replace(/'/g, "\\'") || ''}'
-          };
-          
-          (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/1ir50m1at/1f8t2q5v2';
-            s1.setAttribute('data-api-key', '1778317c1dd8d3c7fca1ae7dea62a7d3dbd060fd');
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-            
-            window.Tawk_API.onLoad = function() {
-              console.log('Tawk.to successfully loaded');
-              Tawk_API.setAttributes({
-                'Company': '${companyData.name?.replace(/'/g, "\\'") || 'Unknown'}',
-                'UserID': '${companyData.id?.replace(/'/g, "\\'") || ''}'
-              }, function(error){});
-            };
-          })();
-        `}
-      </Script>
     </>
   );
 };
